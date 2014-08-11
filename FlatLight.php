5cca2959df4b4d404e6349a2b5b7ee6cc9c5d0e2
Niklas Keller <me@kelunik.com>
2014-08-11 16:21:24 +0200

    debug

--- FlatLight.php
+++ FlatLight.php
@@ -6,10 +6,10 @@ use \Arya\Request as Request;
 use \Arya\Response as Response;
 
 class FlatLight {
-	public function __construct() {
-
+	public function __construct(Request $request) {
+		echo 'ok';
 	}
-	
+
 	public function main(Request $request, Response $response) {
 		$css = file_get_contents(__DIR__ . '/style.css');
 
